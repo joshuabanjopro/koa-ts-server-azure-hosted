04aed859b27b4a382ea8e2c086f99a39679a988e
joshuabanjopro <joshuabanjopro@gmail.com>
2025-11-24 13:06:19 +0000

    Set up dev environment and configs for TS web application

---
+++ build/server.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello World - the TS Way!!!");
+console.log("And now the JS way XD");
+//# sourceMappingURL=server.js.map
---
+++ src/server.ts
@@ -0,0 +1,2 @@
+console.log("Hello World - the TS Way!!!")
+console.log("And now the JS way XD")
